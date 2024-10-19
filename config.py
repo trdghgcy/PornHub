b512b2cd2f58139ed6a84748c4a5f6cb82e07554
trdghgcy <telegrambo.t@outlook.com>
2024-10-19 17:21:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", "")
-ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "")
-REPO_BOT = os.environ.get("REPO_BOT", "https://t.me/spacecloud")
-OWNER = os.environ.get("OWNER", "")
-BOT_NAME = os.environ.get("BOT_NAME", "")
-UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "spacecloud")
+TOKEN = os.environ.get("TOKEN", "7264855002:AAHzIFlUTgNi7Ij95tAjNZia_GpGQqnGMZA")
+ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "VJUWTX-BHPEYN-SGTACG-NNMVSA-ARQ")
+REPO_BOT = os.environ.get("REPO_BOT", "https://t.me/kajal_developer")
+OWNER = os.environ.get("OWNER", "6490007953")
+BOT_NAME = os.environ.get("BOT_NAME", "cc Chemcker Bot")
+UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "kajal_developer")
